be08e2b6cf467e4f1cac183aa85d13302dece7f0
Michal Hoftich <michal.h21@gmail.com>
2018-08-21 12:40:44 +0200

    Retab

--- ctanbib.lua
+++ ctanbib.lua
@@ -4,13 +4,13 @@ kpse.set_program_name("luatex")
 --
 
 if #arg < 1 or arg[1]=="--help" or arg[1]=="-h" then 
-print [[ctanbib - convert ctan package information to bibtex format
-Usage:
+  print [[ctanbib - convert ctan package information to bibtex format
+  Usage:
   texlua ctanbib <package name>
 
-This command will bibtex entry to the terminal output
-]]
-os.exit(1)
+  This command will bibtex entry to the terminal output
+  ]]
+  os.exit(1)
 end
 
 local pkgname = arg[1]
@@ -33,73 +33,76 @@ version = {$version}
 
 local xml = require('luaxml-mod-xml')
 local handler = require('luaxml-mod-handler')
+local dom = require('luaxml-domobject')
+
 
 local load_xml =  function(url)
-    local command = io.popen("wget -qO- ".. url,"r")
+  local command = io.popen("wget -qO- ".. url,"r")
 
-    local info = command:read("*all")
-    command:close()
+  local info = command:read("*all")
+  command:close()
 
-    if string.len(info) == 0 then
-        return false
-    end
-    --print(pkginfo)
-    treehandler = handler.simpleTreeHandler()
-    treehandler.options.noReduce = {authorref=true}
-    x = xml.xmlParser(treehandler)
-    x:parse(info)
-    return treehandler.root
+  if string.len(info) == 0 then
+    return false
+  end
+  --print(pkginfo)
+  treehandler = handler.simpleTreeHandler()
+  treehandler.options.noReduce = {authorref=true}
+  x = xml.xmlParser(treehandler)
+  x:parse(info)
+  return treehandler.root
 end
 
 local get_authors = function(a)
-    local authors = {}
-    local retrieved_authors = {}
-    -- fix LuaXML "feature" 
-    if #a == 0 then a = {a} end
-    for _, author in ipairs(a) do
-      local current = {}
-      current[#current+1] = author._attr.familyname
-      current[#current+1] = author._attr.givenname
-      table.insert(retrieved_authors, table.concat(current, ", "))
-    end
-    return table.concat(retrieved_authors," and ")
+  local authors = {}
+  local retrieved_authors = {}
+  -- fix LuaXML "feature" 
+  if #a == 0 then a = {a} end
+  for _, author in ipairs(a) do
+    local current = {}
+    current[#current+1] = author._attr.familyname
+    current[#current+1] = author._attr.givenname
+    table.insert(retrieved_authors, table.concat(current, ", "))
+  end
+  return table.concat(retrieved_authors," and ")
 end
 
 local get_title = function(title)
-    local title = title:gsub("^(.)", function(a) return unicode.utf8.upper(a) end)
-    return string.format(titleformat, title)
+
+  local title = title:gsub("^(.)", function(a) return unicode.utf8.upper(a) end)
+  return string.format(titleformat, title)
 end
 
 
 local get_url = function(home)
-    local home = home or {}
-    local attr = home["_attr"] or {}
-    local href = attr.href or "http://www.ctan.org/pkg/"..pkgname
-    return href
+  local home = home or {}
+  local attr = home["_attr"] or {}
+  local href = attr.href or "http://www.ctan.org/pkg/"..pkgname
+  return href
 end
 
 local get_version = function(version)
-    local version = version or {}
-    local attr = version["_attr"] or {}
-    return attr["number"], attr["date"]
+  local version = version or {}
+  local attr = version["_attr"] or {}
+  return attr["number"], attr["date"]
 end
 
 local bibtex_escape = function(a)
-    local a = a or ""
-    return a:gsub("([%$%{%}])", function(x) return '\\'..x end)
+  local a = a or ""
+  return a:gsub("([%$%{%}])", function(x) return '\\'..x end)
 end
 
 local compile = function(template, records)
   return template:gsub("$([a-z]+)", function(a) 
-        return bibtex_escape(records[a]) or ""
-    end)
+    return bibtex_escape(records[a]) or ""
+  end)
 end
 
 local entry = load_xml(url)
 
 if not entry then
-    print("Cannot find entry for package "..pkgname)
-    os.exit(1)
+  print("Cannot find entry for package "..pkgname)
+  os.exit(1)
 end
 
 -- root element is also saved, so we use this trick 
